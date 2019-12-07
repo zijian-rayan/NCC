e78a1d31b5520832459aae88c163a50be17565a9
wuzijian6 <zijian.wu@u-psud.fr>
2019-12-07 19:55:14 +0100

    video

--- main.m
+++ main.m
@@ -1,8 +1,16 @@
 clc;
 clear;
 close all;
+
+imgDir  = dir(['*.jpg']);
+for i=1:31
+    Image=imread(imgDir(i).name);
+    Image=rgb2gray(Image);
+
+    
+    
 image2   = rgb2gray(imread('image2.jpg'));
-Image = rgb2gray(imread('image.jpg'));
+%Image = rgb2gray(imread('image.jpg'));
 figure(1)
 subplot(221)
 imshow(Image);
@@ -22,3 +30,4 @@ subplot(224)
 imshow(Image);
 title('image suivi')
 imrect(gca, [xoffSet+1, yoffSet+1, size(image2,2), size(image2,1)]);
+end
